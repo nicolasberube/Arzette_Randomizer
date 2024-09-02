566c2a130a82020725757d3f062de0365dd58db0
Rooby-Roo <thegreenrobby@gmail.com>
2024-09-01 18:22:23 -0600

    add locations.py and location/item name groups

--- Arzette/__init__.py
+++ Arzette/__init__.py
@@ -1,5 +1,6 @@
 from worlds.AutoWorld import WebWorld, World
 from BaseClasses import Region, Location, Item, Tutorial, ItemClassification, MultiWorld
+from locations import all_locations
 
 
 class ArzetteWebWorld(WebWorld):
@@ -14,4 +15,18 @@ class ArzetteLocation(Location):
 class ArzetteWorld(World):
     game: str = "Arzette: The Jewel of Faramore"
     web = ArzetteWebWorld()
-    pass  # todo
+
+    item_name_groups = {
+        "magic": {"Sword Wave", "Smart Gun"},
+        "bombs": {"Bombs", "Bomb Gauntlet"},
+        "blue": {"Blue Magic", "Purple Magic"},
+    }
+    location_name_groups = {
+        "candles": {location for location in all_locations if "Candle" in location.split()},
+        "coins": {location for location in all_locations if "Coin" in location.split()},
+        "jewels": {location for location in all_locations if "Jewel" in location.split()},
+        "plants": {location for location in all_locations if "Plant" in location.split()},
+        "races": {location for location in all_locations if "Race" in location.split()},
+        "rocks": {location for location in all_locations if "Rock" in location.split()},
+        "bags": {location for location in all_locations if "Bag" in location.split()},
+    }
